4414e303c27227bd50555ff0e2002cb3b75887d6
Eddy DEBUISSON <eddydebuisson@gmail.com>
2017-08-02 16:17:51 +0200

    fin Tp4

---
+++ src/main/java/dev/sgp/ecouteur/EcouteurCollaborateur.java
@@ -0,0 +1,33 @@
+package dev.sgp.ecouteur;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import dev.sgp.entite.Collaborateur;
+import dev.sgp.service.CollaborateurService;
+import dev.sgp.util.Constantes;
+
+public class EcouteurCollaborateur implements ServletContextListener {
+	private CollaborateurService collabService = Constantes.COLLAB_SERVICE;
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+
+		Collaborateur collab = new Collaborateur("Debuisson", "SuperJava", "Eddy", LocalDate.now(),
+				"Cité du bois d'épinois", "025546549874616519",
+				"http://www.chatsdesrues.fr/wp-content/uploads/2016/10/acheter-jouet-pour-chat-pas-cher.jpg",
+				ZonedDateTime.now(), true);
+		
+		collabService.sauvegarderCollaborateur(collab);
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+
+	}
+
+
+
+}
